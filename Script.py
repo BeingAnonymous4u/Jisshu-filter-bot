ffb5dabd6473d1cf51c8fc9241d3f30c76703c0f
BeingAnonymous4u <155914718+BeingAnonymous4u@users.noreply.github.com>
2025-01-11 05:09:56 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -113,7 +113,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>{file_name}\n</b>"""
+    FILE_CAPTION = """<b>{file_name}</b>"""
     
 
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
