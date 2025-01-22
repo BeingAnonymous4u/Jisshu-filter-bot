14ed33cdd59f05e1e9dd8da1cc3b6606cae6194a
BeingAnonymous4u <155914718+BeingAnonymous4u@users.noreply.github.com>
2025-01-23 03:11:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,7 +107,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "https://icy-kirbie-storeboton-57ee0752.koyeb.app/")
+URL = environ.get("FQDN", "https://dutch-chandra-rkbotrepos-d2590f99.koyeb.app/")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
