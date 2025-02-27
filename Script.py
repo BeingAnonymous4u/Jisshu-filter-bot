77ac7b861e7cc34d2f5045f01479482bbace97b1
BeingAnonymous4u <155914718+BeingAnonymous4u@users.noreply.github.com>
2025-02-27 16:53:38 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -115,7 +115,13 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>{file_name}</b>"""
+    FILE_CAPTION = """<b><u>Fɪʟᴇɴᴀᴍᴇ</u> : </b><b>@hdhindibackup</b><b>  | {file_name}
+    
+</b><b><u>FɪʟᴇSize</u> : {file_size}
+
+</b><b><u>𝗟𝗲𝘁𝗲𝘀𝘁 𝗥𝗲𝗹𝗲𝗮𝘀𝗲 </u> 
+👉 </b><b>https://t.me/IpopcornsMovies</b><b>
+👉 </b><b>https://t.me/IpopcornsMovies</b>"""
     
 
     ALRT_TXT = """Search for yourself !"""
