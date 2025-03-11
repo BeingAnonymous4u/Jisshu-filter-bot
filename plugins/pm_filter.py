1e1467b917fb43bc26e7298f056afe9dad0b8bb8
BeingAnonymous4u <155914718+BeingAnonymous4u@users.noreply.github.com>
2025-03-11 21:45:21 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1450,18 +1450,21 @@ async def auto_filter(client, msg, spoll=False , pm_mode = False):
         for file_num, file in enumerate(files, start=1):
             links += f"""<b>\n\n{file_num}. <a href=https://t.me/{temp.U_NAME}?start={"pm_mode_" if pm_mode else ''}file_{ADMINS[0] if pm_mode else message.chat.id}_{file.file_id}>{get_size(file.file_size)} | {formate_file_name(file.file_name)}</a></b>"""
     else:
-        btn = [[InlineKeyboardButton(text=f"🔗 {get_size(file.file_size)} ◾ {formate_file_name(file.file_name)}", url=f'https://telegram.dog/{temp.U_NAME}?start=file_{message.chat.id}_{file.file_id}'),]
+        btn = [[InlineKeyboardButton(text=f"🔗 {get_size(file.file_size)} ▪️ {formate_file_name(file.file_name)}", url=f'https://telegram.dog/{temp.U_NAME}?start=file_{message.chat.id}_{file.file_id}'),]
                for file in files
               ]
     if offset != "":
         if total_results >= MAX_BTN:
             btn.insert(0,[
-	InlineKeyboardButton("🗣️ ᴄʜᴏᴏsᴇ ʟᴀɴɢᴜᴀɢᴇ 🗣️", callback_data=f"languages#{key}#{offset}#{req}"),
+	InlineKeyboardButton("🗣️ Cʜᴏᴏsᴇ Lᴀɴɢᴜᴀɢᴇ 🗣️", callback_data=f"languages#{key}#{offset}#{req}"),
         ])
             btn.insert(1, [
-        InlineKeyboardButton("✨ ǫᴜᴀʟɪᴛʏ 🎊", callback_data=f"qualities#{key}#{offset}#{req}"),
-	InlineKeyboardButton("🎭 sᴇᴀsᴏɴ 🏖️", callback_data=f"seasons#{key}#{offset}#{req}")
-      ])    
+        InlineKeyboardButton("✨ Qᴜᴀʟɪᴛʏ 🤤", callback_data=f"qualities#{key}#{offset}#{req}"),
+	InlineKeyboardButton("🎭 Sᴇᴀsᴏɴ 🎬", callback_data=f"seasons#{key}#{offset}#{req}")
+        ])
+            btn.insert(2,[
+        InlineKeyboardButton("⇈ Sᴇʟᴇᴄᴛ Oᴘᴛɪᴏɴ Hᴇʀᴇ ⇈", 'reqinfo')
+        ])    
         else:
             btn.insert(0,[
                 InlineKeyboardButton("🗣️ ᴄʜᴏᴏsᴇ ʟᴀɴɢᴜᴀɢᴇ 🗣️", callback_data=f"languages#{key}#{offset}#{req}")
